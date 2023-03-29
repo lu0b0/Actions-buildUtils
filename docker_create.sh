42ad5964d5a8e9e9d55425ed2c0afaf81e7a1f4c
萝卜 <106028210+lu0b0@users.noreply.github.com>
2023-03-29 23:02:45 +0800

    Update docker_create.sh

--- docker_create.sh
+++ docker_create.sh
@@ -1,4 +1,2 @@
-mkdir /app
-wget https://ghproxy.com/https://github.com/lu0b0/ELM/releases/download/3.0/radish -O /app/radish
-wget https://ghproxy.com/https://github.com/lu0b0/radish/raw/master/Config.json -O /app/Config.json
-chmod -R 777 /app
+wget https://ghproxy.com/https://github.com/lu0b0/ELM/releases/download/3.0/radish
+wget https://ghproxy.com/https://github.com/lu0b0/radish/raw/master/Config.json
